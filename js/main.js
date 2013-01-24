34436975fb5d7eeca60dfbabda7a50debeaf9813
joamag <joamag@gmail.com>
2013-01-24 11:31:42 +0000

    new support for checksum in scan

--- js/main.js
+++ js/main.js
@@ -312,7 +312,7 @@
          * The length of the code to be scanned this value should be defined in
          * accordance with the defined specification.
          */
-        var SCAN_CODE_LENGTH = 18;
+        var SCAN_CODE_LENGTH = 22;
 
         /**
          * The list of integer based versions that are compatible with the
@@ -347,13 +347,24 @@
                         return;
                     }
 
+                    // retrieves the checksum for the barcode value
+                    // in order to verify it against the base buffer
+                    // value and in case the verification fails returns
+                    // immediately (not a valid barcode)
+                    var checksumS = value.slice(0, 4);
+                    var buffer = value.slice(4);
+                    var _checksumS = checksum(buffer);
+                    if (_checksumS != checksumS) {
+                        return;
+                    }
+
                     // retrieves the version of the barcode then
                     // retrieves the class of the object that is
                     // represented by the barcode and then retrieves
                     // the identifier of the object
-                    var version = value.slice(0, 2);
-                    var classId = value.slice(2, 6);
-                    var objectId = value.slice(6);
+                    var version = value.slice(4, 6);
+                    var classId = value.slice(6, 10);
+                    var objectId = value.slice(10);
 
                     // converts the version into an integer
                     // to be used in the resolution and verifies that
@@ -382,7 +393,7 @@
                     // provided scan value is compatible with the current
                     // scan version (version is included in compatible
                     // version set) in case it's not returns immediately
-                    var isCompatible = COMPATIBLE_VERSIONS.indexof(versionInt) != -1;
+                    var isCompatible = COMPATIBLE_VERSIONS.indexOf(versionInt) != -1;
                     if (!isCompatible) {
                         return;
                     }
@@ -427,6 +438,38 @@
         // to be able to react to it
         _document.bind("scan_error", function(event, value) {
                 });
+
+        var checksum = function(buffer, modulus, salt) {
+            // retrieves the various value for the provided
+            // parameters defaulting to base valus in case
+            // the values were not provided
+            modulus = modulus || 10000;
+            salt = salt || "omni";
+
+            // creates the complete buffer value by adding
+            // the salt value to the buffer and starts the
+            // checksum counter value to zero
+            var _buffer = buffer + salt;
+            var counter = 0;
+
+            // iterates over all the bytes in the buffer
+            // to append their ordinal values to the counter
+            // note that a left shift is done according to
+            // the position of the byte
+            for (var index = 0; index < _buffer.length; index++) {
+                var _byte = _buffer[index];
+                var byteI = _byte.charCodeAt(0);
+                counter += byteI << index;
+            }
+
+            // retrieves the checksum as an integer with the modulus
+            // operation on the current counter value, then converts
+            // the value into a string and returns it to the caller
+            // method as the final checksum value
+            var checksum = counter % modulus;
+            var checksumS = String(checksum);
+            return checksumS;
+        };
     };
 })(jQuery);
 
