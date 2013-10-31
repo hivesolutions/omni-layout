a92480a75c187c91ab85e22346f5c994eee8c917
joamag <joamag@gmail.com>
2013-10-31 15:49:57 +0000

    new main values

--- js/main.js
+++ js/main.js
@@ -1254,6 +1254,32 @@
 })(jQuery);
 
 (function(jQuery) {
+    jQuery.fn.uactivity = function(options) {
+        var matchedObject = jQuery(this);
+
+        matchedObject.each(function(index, element) {
+                    var _element = jQuery(this);
+
+                    var items = jQuery("> li", _element);
+
+                    items.each(function(index, element) {
+                                var _element = jQuery(this);
+                                var data = jQuery(".data", _element);
+                                var message = jQuery(".message", _element);
+
+                                var dataS = data.text();
+                                var dataJ = jQuery.parseJSON(dataS);
+
+                                console.info(dataJ);
+
+                                var message = dataJ["message"];
+                                message = jQuery.uxlocale(message);
+                            });
+                })
+    };
+})(jQuery);
+
+(function(jQuery) {
     var BUNDLE_EN_US = {
         "payment_method:cash" : "Cash",
         "payment_method:card" : "Card",
