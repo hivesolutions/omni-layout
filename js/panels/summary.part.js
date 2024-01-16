4180130a67b8212c090cad47d32df049028608b6
João Magalhães <joamag@gmail.com>
2024-01-16 11:58:45 +0000

    chore: small spell fix

--- js/panels/summary.part.js
+++ js/panels/summary.part.js
@@ -17,7 +17,7 @@
 
         // in case the matched object is not defined
         // or in case it's an empty list must return
-        // immediatly initialization is not meant to
+        // immediately initialization is not meant to
         // be run (corruption may occur)
         if (!matchedObject || matchedObject.length === 0) {
             return;
